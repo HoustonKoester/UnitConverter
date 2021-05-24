652bf83c8d3bd4680e3fef0c1c90ddce902b257f
Houston <houstonkoester@aol.com>
2021-05-24 12:36:22 -0700

    cleaned up unneeded code

--- src/main/Converter.java
+++ src/main/Converter.java
@@ -19,7 +19,7 @@ public class Converter {
 		
 		//main menu repeatable selector, end main with input of 9 spaces 5-8 left for other conversion types
 		while(true) {
-			sysout("Please select an option:\n1. Time\n2. Distances\n3. Temperatures\n4. Mass\n9. Close the program");
+			sysout("Please select an option:\n1. Energy\n2. Distances\n3. Temperatures\n4. Mass\n9. Close the program");
 			int menuSelect = s.nextInt();
 			switch(menuSelect) {
 			
@@ -29,28 +29,24 @@ public class Converter {
 					sysout("Please select from the energy converter menu or enter 4 to return to the main menu\n1. Joules to BTU (British Thermal Units)\n2. Joules to Foot-Pounds\n3. Joules to Calories\n4. Return to main menu");
 					int energyselect = energy.nextInt();
 					double joules;
-					double convert;
 					switch(energyselect) {
 					
 					case 1: //joules to BTU
 						sysout("Please enter how many joules you want to convert to BTU (British Thermal Units): ");
 						joules = energy.nextDouble();
-						convert = CF.joulToBTU(joules);
-						sysout(joules + " Joules to British Thermal Units is " + convert + " BTU.");
+						sysout(joules + " Joules to British Thermal Units is " + CF.joulToBTU(joules) + " BTU.");
 						break;
 					
 					case 2://joules to FootPound
 						sysout("Please enter how many joules you want to convert to foot-pounds: ");
 						joules = energy.nextDouble();
-						convert = CF.joulToFP(joules);
-						sysout(joules + " Joules to Foot-Pounds is " + convert + " Foot-Pounds.");
+						sysout(joules + " Joules to Foot-Pounds is " + CF.joulToFP(joules) + " Foot-Pounds.");
 						break;
 						
 					case 3: //joules to calories
 						sysout("Please enter how many joules you want to convert to calories: ");
 						joules = energy.nextDouble();
-						convert = CF.joulToCal(joules);
-						sysout(joules + " Joules to Calories is " + convert + " calories.");
+						sysout(joules + " Joules to Calories is " + CF.joulToCal(joules) + " calories.");
 						break;
 						
 					case 4: //main return break from menu loop
@@ -72,28 +68,24 @@ public class Converter {
 					sysout("Please select from the distance converter menu or enter 4 to return to the main menu\n1. Meters to Miles\n2. Meters to Feet\n3. Meters to Inches\n4. Return to main menu");
 					int distselect = dist.nextInt();
 					double meters;
-					double conversion;
 					switch(distselect) {
 					
 					case 1: //meters to miles
 						sysout("Please enter how many meters you want to convert to miles: ");
 						meters = dist.nextDouble();
-						conversion = CF.meterToMile(meters);
-						sysout(meters + " meters to miles is " + conversion + " miles.");
+						sysout(meters + " meters to miles is " + CF.meterToMile(meters) + " miles.");
 						break;
 					
 					case 2: //meters to feet
 						sysout("Please enter how many meters you want to convert to feet: ");
 						meters = dist.nextDouble();
-						conversion = CF.meterToFeet(meters);
-						sysout(meters + " meters to feet is " + conversion + " feet.");
+						sysout(meters + " meters to feet is " + CF.meterToFeet(meters) + " feet.");
 						break;
 						
 					case 3: //meters to inches
 						sysout("Please enter how many meters you want to convert to inches: ");
 						meters = dist.nextDouble();
-						conversion = CF.meterToInch(meters);
-						sysout(meters + " meters to inches is " + conversion + " inches.");
+						sysout(meters + " meters to inches is " + CF.meterToInch(meters) + " inches.");
 						break;
 						
 					case 4: //main return break from menu loop
@@ -113,29 +105,25 @@ public class Converter {
 				while(true) {
 					sysout("Please select from the temperature converter menu or enter 4 to return to the main menu\n1. Celsuis to Fahrenheit\n2. Celsuis to Kelvin\n3. Celsuis to Rankine\n4. Return to main menu");
 					int tempselect = temp.nextInt();
-					double temps;
 					double celsuius;
 					switch(tempselect) {
 					
 					case 1: // celsuis to fehrenheit
 						sysout("Please enter how many degrees in Celsuis you want to convert to Fahrenheit: ");
 						celsuius = temp.nextDouble();
-						temps = CF.celsToFahren(celsuius);
-						sysout(celsuius + " in Celsuis is " + temps + " degrees in Fahrenheit.");
+						sysout(celsuius + " in Celsuis is " + CF.celsToFahren(celsuius) + " degrees in Fahrenheit.");
 						break;
 					
 					case 2: // celsuis to kelvin
 						sysout("Please enter how many degrees in Celsuis you want to convert to Kelvin: ");
 						celsuius = temp.nextDouble();
-						temps = CF.celsToKelvin(celsuius);
-						sysout(celsuius + " in Celsuis is " + temps + " degrees in Kelvin.");
+						sysout(celsuius + " in Celsuis is " + CF.celsToKelvin(celsuius) + " degrees in Kelvin.");
 						break;
 						
 					case 3: // celsuis to Rankine
 						sysout("Please enter how many degrees in Celsuis you want to convert to Rankine: ");
 						celsuius = temp.nextDouble();
-						temps = CF.celsToRankine(celsuius);
-						sysout(celsuius + " in Celsuis is " + temps + " degrees in Rankine.");
+						sysout(celsuius + " in Celsuis is " + CF.celsToRankine(celsuius) + " degrees in Rankine.");
 						break;
 						
 					case 4: 
@@ -158,28 +146,24 @@ public class Converter {
 					sysout("Please select from the mass converter menu or enter 4 to return to the main menu\n1. Grams to Stone\n2. Grams to Pound\n3. Grams to Ounce\n4. Return to main menu");
 					int massSelect = mass.nextInt();
 					double grams;
-					double weight;
 					switch(massSelect) {
 					
 					case 1: //gram to stone
 						sysout("Please enter how many Grams you want to convert to Stone weight: ");
 						grams = mass.nextDouble();
-						weight = CF.gramToStone(grams);
-						sysout(grams + " in Grams is " + weight + " Stone in weight.");
+						sysout(grams + " in Grams is " + CF.gramToStone(grams) + " Stone in weight.");
 						break;
 					
 					case 2: //gram to pound
 						sysout("Please enter how many Grams you want to convert to Pounds weight: ");
 						grams = mass.nextDouble();
-						weight = CF.gramToPound(grams);
-						sysout(grams + " in Grams is " + weight + " Pounds in weight.");
+						sysout(grams + " in Grams is " + CF.gramToPound(grams) + " Pounds in weight.");
 						break;
 						
 					case 3: //gram to ounce
 						sysout("Please enter how many Grams you want to convert to Ounces weight: ");
 						grams = mass.nextDouble();
-						weight = CF.gramToOunce(grams);
-						sysout(grams + " in Grams is " + weight + " Ounces in weight.");
+						sysout(grams + " in Grams is " + CF.gramToOunce(grams) + " Ounces in weight.");
 						break;
 						
 					case 4:
